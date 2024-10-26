89934f9f3203c5d159aec40a5556f96891173f6b
Danisse Cabaña <cabanadanisse7@gmail.com>
2024-10-26 21:58:46 +0800

    Initial Commit of Login and Index

--- PagePulse/main/urls.py
+++ PagePulse/main/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
-from .views import index, login_view
+from django.contrib import admin
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('login/', login_view, name='login'),
+    path('', views.index, name='index'),  # Make sure to include the namespace if you're using it
+    path('login/', views.login_view, name='login'),
 ]
