be680051391bd9651c70fb9fb217ea16f9d1741f
Danisse Cabaña <cabanadanisse7@gmail.com>
2024-10-27 15:01:33 +0800

    Initial Commit of models.py, admin.py

--- PagePulse/main/admin.py
+++ PagePulse/main/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_date', 'category', 'rating_mature')
+    search_fields = ('title', 'author')
+    list_filter = ('category', 'rating_mature')
+
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'author', 'published_date', 'book_image')
+        }),
+        ('Details', {
+            'fields': ('description', 'characters', 'category', 'target_audience', 'language', 'tags', 'rating_mature')
+        }),
+    )
+
+admin.site.register(Book, BookAdmin)
--- PagePulse/main/models.py
+++ PagePulse/main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField()
+    # book_image = models.ImageField(upload_to='book_images/', blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    characters = models.TextField(blank=True, null=True)
+    category = models.CharField(max_length=100, blank=True, null=True)
+    target_audience = models.CharField(max_length=100, blank=True, null=True)
+    language = models.CharField(max_length=50, blank=True, null=True)
+    tags = models.CharField(max_length=200, blank=True, null=True)
+    rating_mature = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
