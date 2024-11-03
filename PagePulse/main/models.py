4358829678800e1434e89365acda706dd6b64603
ezzeljan <ezzelfrancisco95@gmail.com>
2024-11-03 20:45:51 +0800

    Added UserLibrary in models

--- PagePulse/main/models.py
+++ PagePulse/main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -16,3 +17,7 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
+class UserLibrary(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    added_date = models.DateField(auto_now_add=True)
