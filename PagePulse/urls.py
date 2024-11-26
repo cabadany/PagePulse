a1e03b5b2ba2f75519669cec215150cba45d1bf4
Danisse Cabaña <cabanadanisse7@gmail.com>
2024-11-26 20:42:22 +0800

    Initial Commit of views.py, index.html, urls.py

--- PagePulse/urls.py
+++ PagePulse/urls.py
@@ -14,9 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.urls import path
+from .login_registration import views as login_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('login/', views.login_view, name='login'),
+    path('signup/', views.signup_view, name='signup'),
+    path('new_story/', views.new_story, name='new_story'),
+    path('home/', views.homepage, name='homepage'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- login_registration/views.py
+++ login_registration/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Welcome to the PagePulse homepage!")
+
+def signup_view(request):
+    return HttpResponse("Signup page")
+
+def login_view(request):
+    return HttpResponse("Login page")
