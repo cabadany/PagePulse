07b0f032a8c97d0303c0f729ec84443e3f1d9732
Gianne Andrea Garcia <gianneandrea.garcia@cit.edu>
2024-11-28 00:57:30 +0800

    Initial Commit of SignUp function

--- login_registration/apps.py
+++ login_registration/apps.py
@@ -1,6 +1,10 @@
-from django.apps import AppConfig
+# login_registration/apps.py
 
+from django.apps import AppConfig
 
 class LoginRegistrationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'login_registration'
+    name = 'login_registration'  # Ensure this matches the app name in INSTALLED_APPS
+
+    def ready(self):
+        import login_registration.signals  # Correct import path
