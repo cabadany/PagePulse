456a3bf11f3c8cd44a09b233dd5b02fc9db8939b
Gianne Andrea Garcia <gianneandrea.garcia@cit.edu>
2024-11-28 01:13:04 +0800

    Initial commit of SignUp(Error message)

--- login_registration/apps.py
+++ login_registration/apps.py
@@ -1,10 +1,8 @@
 # login_registration/apps.py
-
 from django.apps import AppConfig
 
 class LoginRegistrationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'login_registration'  # Ensure this matches the app name in INSTALLED_APPS
+    name = 'login_registration'
 
     def ready(self):
-        import login_registration.signals  # Correct import path
+        import login_registration.signals  # Ensure signals are imported
--- login_registration/signals.py
+++ login_registration/signals.py
@@ -1,14 +1,14 @@
-
+# login_registration/signals.py
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile  # Ensure you import the Profile model
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
-    if created:
+    if created:  # Only create profile if the user is created
         Profile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
+    instance.profile.save()  # Save the profile whenever the user is saved
