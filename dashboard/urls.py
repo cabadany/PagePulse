43972f1929256535c0138ce6dc901dfccb986202
Danisse Cabaña <cabanadanisse7@gmail.com>
2024-11-27 23:36:43 +0800

    Intial Commit of urls.py, views.py, homepage.html

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('read/<int:book_id>/', views.read_book, name='read_book'),
     path('read/<int:book_id>/<int:chapter_id>/', views.read_book, name='read_specific_chapter'),
+    path('category/<str:category>/', views.category_view, name='category_view'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -8,6 +8,14 @@ def homepage(request):
         'books': books,
     })
 
+def category_view(request, category):
+    if category == 'All':
+        books = Book.objects.all()
+    else:
+        books = Book.objects.filter(genre=category)
+    
+    return render(request, 'category_page.html', {'category': category, 'books': books})
+
 def read_book(request, book_id, chapter_id=None):
     book = Book.objects.get(id=book_id)
     chapters = book.chapters.all().order_by('id')
