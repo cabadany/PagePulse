72118ecaf9c5eea11596fb7f0d422b7ca9f3eaa3
Gianne Andrea Garcia <gianneandrea.garcia@cit.edu>
2024-11-27 11:38:48 +0800

    Initial Commit of digitcode.html

--- forgot_password/urls.py
+++ forgot_password/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.forgot_password_view, name='forgotpassword'),
+    path('digitcode/', views.digitcode_view, name='digitcode'),
+    path('resetpassword/', views.reset_password_view, name='resetpassword'),
 ]
--- forgot_password/views.py
+++ forgot_password/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 
 def forgot_password_view(request):
     return render(request, 'forgotpassword.html')
+
+def digitcode_view(request):
+    # Your view logic here
+    return render(request, 'digitcode.html')
+
+def reset_password_view(request):
+    # Your logic for resetting password
+    return render(request, 'resetpassword.html')
