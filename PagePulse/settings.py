9dfbe63f20fc78d60bb5dae1ca71ae6627bb4d34
Danisse Cabaña <cabanadanisse7@gmail.com>
2024-11-27 23:45:35 +0800

    Initial Commit of category_page.html, homepage.html, settings.py

--- PagePulse/settings.py
+++ PagePulse/settings.py
@@ -13,6 +13,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
+# For static files (CSS, JS, etc.)
+STATIC_URL = '/static/'
+
+# For media files (uploads like images)
+MEDIA_URL = '/media/'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
