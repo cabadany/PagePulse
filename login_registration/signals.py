f6722403c6ee95898fafc0d4127b6684c3a71fa9
Danisse Cabaña <cabanadanisse7@gmail.com>
2024-11-28 15:40:49 +0800

    Initial Commit of apps.py, signals.py

--- login_registration/signals.py
+++ login_registration/signals.py
@@ -1,13 +1,14 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
-from .models import Profile  # Ensure you import the Profile model
+from .models import Profile
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
-    if created:  # Only create profile if the user is created
+    if created and not hasattr(instance, 'profile'):
         Profile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()  # Save the profile whenever the user is saved
+    if hasattr(instance, 'profile'):
+        instance.profile.save()
