3737361ac0bbd980924baeafc51b3d7455a61b6e
Danisse Cabaña <cabanadanisse7@gmail.com>
2024-11-30 14:10:31 +0800

    Initial Commit of forms.py, urls.py, views.py, content_box.html, new_story.html, new_chapter.html

--- book/urls.py
+++ book/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('new_story/', views.new_story, name='new_story'),
-    path('my_stories/', views.my_stories, name='my_stories'),
-]
+    path('my-stories/', views.my_stories, name='my_stories'),
+    path('new-story/', views.new_story, name='new_story'),
+    path('new-chapter/', views.content_box, name='content_box'),
+    path('my-stories/content-box/<int:book_id>/', views.content_box, name='new_chapter'),
+    path('my-stories/content-box/<int:book_id>/', views.content_box, name='content_box'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- book/views.py
+++ book/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import BookForm
+from .models import Chapter, Book
 
 def my_stories(request):
     return render(request, 'my_stories.html')
 
+def new_chapter(request):
+    return render(request, 'new_chapter.html')
+
 def new_story(request):
-    return render(request, 'new_story.html')
+    if request.method == 'POST':
+        form = BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            book = form.save()
+            return redirect('content_box', book_id=book.id)
+    else:
+        form = BookForm()
+
+    return render(request, 'new_story.html', {'form': form})
+
+def content_box(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+
+        Chapter.objects.create(book=book, title=title, content=content)
+
+        return redirect('homepage')  
+
+    return render(request, 'content_box.html', {'book': book})
