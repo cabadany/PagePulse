3b7cd5ef7e49ceb516baa0ed1707aa6f9194b8b3
Gianne Andrea Garcia <gianneandrea.garcia@cit.edu>
2024-11-30 21:05:05 +0800

    Initial commit of password_reset_email

--- forgot_password/urls.py
+++ forgot_password/urls.py
@@ -14,8 +14,9 @@ urlpatterns = [
          name='password_reset_done'),
     
     path('reset/<uidb64>/<token>/', 
-         auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), 
-         name='password_reset_confirm'),
+     auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), 
+     name='password_reset_confirm'),
+
     
     path('reset/done/', 
          auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), 
--- forgot_password/views.py
+++ forgot_password/views.py
@@ -1,10 +1,22 @@
-from django.contrib.auth.views import PasswordResetConfirmView
-from django.urls import reverse_lazy
-from django.contrib.auth.forms import SetPasswordForm
+from django.contrib.auth.views import PasswordResetView
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 
+class CustomPasswordResetView(PasswordResetView):
+    email_template_name = 'password_reset_email.html'  # Custom email template
+    subject_template_name = 'password_reset_subject.txt'  # Optional custom subject template
+    
+    def send_mail(self, subject_template_name, email_template_name, context, from_email, to_email, **kwargs):
+        # Render subject and email content
+        subject = render_to_string(subject_template_name, context).strip()
+        message = render_to_string(email_template_name, context)
 
-class CustomPasswordResetConfirmView(PasswordResetConfirmView):
-    template_name = 'password_reset_confirm.html'
-    form_class = SetPasswordForm
-    success_url = reverse_lazy('password_reset_complete')
-
+        # Send the email
+        send_mail(
+            subject,
+            message,
+            from_email,
+            [to_email],
+            **kwargs
+        )
