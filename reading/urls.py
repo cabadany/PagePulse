ded6faaa18bdf5fbcdc9cb5538d39f5c7d56fda4
ezzeljan <ezzelfrancisco95@gmail.com>
2024-12-02 10:22:40 +0800

    Initial commit of private_libray.html

--- reading/urls.py
+++ reading/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('read/<int:book_id>/', views.read_book, name='read_book'),
     path('read/<int:book_id>/<int:chapter_id>/', views.read_book, name='read_specific_chapter'),
+    path('update_progress/', views.update_progress, name='update_progress'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
